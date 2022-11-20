e018c06b8b2b0ee55a8f8f6fa62d951a814dd7f1
CaptureB <airdavid62@gmail.com>
2022-11-20 11:02:33 -0700

    Started script file, added variables

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    //Making Variables, API key for Open Weather//
+
+    const APIkey = "0a71ef47646bdf99202bf67a17049362"
+    let city = $("h2#city");
+    let day = $("h3#day");
+    let temperature = $("span#temprature");
+    let humidity = $("span#humidty");
+    let windSpeed = $("span#wind");
+    let listCity = $("div.listCity");
+
+    let searchCity = $("#search-city");
+
+    let previousCity = [];
+}
